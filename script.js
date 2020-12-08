d1b7ea1796c7b299a13b3868dc0d030f98036aa6
Shkel Nikita <shadowkael.stream@gmail.com>
2020-12-08 16:50:21 +0300

    Домашнее задание. В папке 3 задание со звездочкой

--- script.js
+++ script.js
@@ -1,17 +1,76 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-  ];
-  
-  const renderGoodsItem = (title = '', price = 0) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-  };
-  
-  const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
+class Bucket{
+  constructor(items = [] , cost = 0){
+    this.items = items;
+    this.cost = cost;
   }
-  
-  renderGoodsList(goods);
+
+  addItem(item){}
+  removeItem(item){}
+  calcSumm(){}
+}
+
+class BucketItem extends ProductItem{}
+
+class ProductList{
+  constructor(container = '.goods-list'){
+    this.container = container;
+    this.goods = [];
+    this.allProducts = [];
+    this._fetchProducts();
+    this._render();
+  }
+
+  _fetchProducts(){
+    this.goods = [
+      {id: 1, title: 'Notebook', price: 20000 },
+      {id: 2, title: 'Mouse', price: 1500 },
+      {id: 3, title: 'Keyboard', price: 5000 },
+      {id: 4, title: 'Gamepad', price: 4500 },
+    ];
+  }
+
+  _render(){
+    const block = document.querySelector(this.container);
+
+    for(let product of this.goods){
+      const productObject = new ProductItem(product);
+      this.allProducts.push(productObject);
+      block.insertAdjacentHTML('beforeend', productObject.render())
+    }
+  }
+
+  calcSumm(){
+    let summPrice = 0;
+
+    for(let price of this.goods){
+      summPrice += price.price
+    }
+
+    return summPrice;
+  }
+}
+
+
+
+class ProductItem{
+  constructor(product, img = 'https://placehold.it/200x150'){
+    this.title = product.title;
+    this.price = product.price;
+    this.id = product.id;
+    this.img = img;
+  }
+
+  render(){
+    return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some Img">
+                <div class="desc">
+                  <h3>${this.title}
+                  <p>${this.price} \u20bd</p>
+                  <button class="buy-btn">Купить</button>
+                </div>
+              </div>`;
+  }
+}
+
+const list = new ProductList();
+console.log(list.calcSumm())
